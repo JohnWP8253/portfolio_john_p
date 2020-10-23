5c24a660a0eae8db79ea8fcb7dc0f64769187fc5
John Pendergrass <john.pendergrass@me.com>
2020-10-23 15:04:54 -0500

    established linkes in the navbar and the drawer

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import './App.css';
-import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
-import Main from './components/Main';
-
+import "./App.css";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Main from "./components/Main";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-         <Layout>
+      <Layout>
         <Header title="John Pendergrass" scroll>
-            <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-            </Navigation>
+          <Navigation>
+            <Link to="/resume">Resume</Link>
+            <Link to="/aboutMe">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
         </Header>
-        <Drawer title="Title">
-            <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-            </Navigation>
+        <Drawer title="John Pendergrass">
+          <Navigation>
+            <Link to="/resume">Resume</Link>
+            <Link to="/aboutMe">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
         </Drawer>
         <Content>
-            <div className="page-content" />
-            <Main />
+          <div className="page-content" />
+          <Main />
         </Content>
-    </Layout>
+      </Layout>
     </div>
   );
 }
