cee78b303ee203183980857f1564992ce131f866
John Pendergrass <john.pendergrass@me.com>
2020-11-19 07:09:34 -0600

    added base carousel code and first slide image to images

--- src/components/Navbar/MyNavbar.jsx
+++ src/components/Navbar/MyNavbar.jsx
@@ -21,12 +21,14 @@ export const MyNavbar = () => {
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <Navbar.Collapse id='responsive-navbar-nav'>
           <Nav className='ml-auto'>
+            {/* -------------------------------------------------------- */}
+            {/* Links for navigation to different parts of the portfolio */}
+            {/* -------------------------------------------------------- */}
             <Nav.Link href='#home'>Home</Nav.Link>
             <Nav.Link href='#about'>About</Nav.Link>
             <Nav.Link href='#skills'>Skills</Nav.Link>
             <Nav.Link href='#experience'>Experience</Nav.Link>
             <Nav.Link href='#Projects'>Projects</Nav.Link>
-            <Nav.Link href='#Resume'>Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
