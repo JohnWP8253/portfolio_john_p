2761fc630458f1f424c7302ca3fafd90768d8e8c
John Pendergrass <john.pendergrass@me.com>
2020-11-19 09:10:31 -0600

    created MyCarousel, added images, removed unneeded code, added ScrollDown component

--- src/components/MyCarousel/MyCarousel.component.jsx
+++ src/components/MyCarousel/MyCarousel.component.jsx
@@ -1,49 +1,39 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import Slide1 from "../../assets/images/IMG_0261.jpeg";
+import Slide2 from "../../assets/images/IMG_0620.jpg";
+import Slide3 from "../../assets/images/IMG_1613.jpeg";
+import ScrollDown from "../Scroll-down/scrollDown.component";
 
 const MyCarousel = () => {
   return (
-    <>
-      <Carousel>
+    <div id="home">
+      <Carousel controls={false} indicators interval={2500} pause={false}>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
+            id="slide1"
+            className="d-block w-100 custom-img"
+            src={Slide1}
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Third slide"
+            className="d-block w-100 custom-img"
+            src={Slide2}
+            alt="Second slide"
           />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
+            className="d-block w-100 custom-img"
+            src={Slide3}
             alt="Third slide"
           />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-    </>
+      <ScrollDown />
+    </div>
   );
 };
 
