d914ecde037be98ebef6e60e1cf7a6cf197ce8a5
John Pendergrass <john.pendergrass@me.com>
2020-11-30 14:33:54 -0600

    added data file for cards

--- src/pages/Skills/Skills.component.jsx
+++ src/pages/Skills/Skills.component.jsx
@@ -11,6 +11,11 @@ const Skills = () => {
   return (
     <div className="pt-3 pb-3" id="skills">
       <h1 className="text-center font-details-b pb-4">TECH SKILLS</h1>
+      <CardDeck>
+          <Row className="d-flex justify-content-around">
+              
+          </Row>
+      </CardDeck>
     </div>
   );
 };
